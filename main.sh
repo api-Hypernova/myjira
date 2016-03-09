f141fd1c6e950bb2fe246c6f0499d367c8e0188f
api-Hypernova <sdlparadigm@sdf.org>
2016-03-08 21:27:56 -0500

    added some notes

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#we create a few basic commands like add <jira>, edit, remove, close, resolve, comment, assign, etc
+
+#the interface should be interactive and not outside the program. You should enter commands at a prompt like gdb or something 
+
+#A Jira should be a export file which contains all the goodies. these commands should append/edit/rewrite information. 
+
+#sync should be done through github itself. Each command should first pull, edit, then push. Random numbers can be generated through unix timestamps etc
+
+#there should be a main while loop blocked by a read command 
+
+#the script should be able to push/pull without password. 
+
+#there should be an installer that drops this in your bin so you can call it anywhere Remember to keep all paths absolute
+
